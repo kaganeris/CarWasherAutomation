0512cbac1a111ed663561ef8157e46d68c35721f
Kağan Eriş <kaan.ers@hotmail.com>
2023-09-26 13:46:19 +0300

    main menu update

--- Program.UI/Forms/MainMenu.Designer.cs
+++ Program.UI/Forms/MainMenu.Designer.cs
@@ -30,19 +30,11 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenu));
             panel1 = new Panel();
-            btnCustomers = new Button();
-            pictureBox8 = new PictureBox();
-            pictureBox4 = new PictureBox();
-            pictureBox7 = new PictureBox();
-            pictureBox2 = new PictureBox();
-            btnVehicles = new Button();
-            btnEmployees = new Button();
             btnDashboard = new Button();
+            btnEmployees = new Button();
+            btnVehicles = new Button();
+            btnCustomers = new Button();
             panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // panel1
@@ -52,97 +44,11 @@
             panel1.Controls.Add(btnEmployees);
             panel1.Controls.Add(btnVehicles);
             panel1.Controls.Add(btnCustomers);
-            panel1.Controls.Add(pictureBox8);
-            panel1.Controls.Add(pictureBox4);
-            panel1.Controls.Add(pictureBox7);
-            panel1.Controls.Add(pictureBox2);
             panel1.Dock = DockStyle.Left;
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
             panel1.Size = new Size(340, 846);
             panel1.TabIndex = 0;
-            // 
-            // btnCustomers
-            // 
-            btnCustomers.BackColor = Color.Transparent;
-            btnCustomers.FlatAppearance.BorderSize = 0;
-            btnCustomers.FlatStyle = FlatStyle.Flat;
-            btnCustomers.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
-            btnCustomers.ForeColor = Color.Transparent;
-            btnCustomers.Location = new Point(100, 235);
-            btnCustomers.Name = "btnCustomers";
-            btnCustomers.Size = new Size(192, 50);
-            btnCustomers.TabIndex = 2;
-            btnCustomers.Text = "Customers";
-            btnCustomers.UseVisualStyleBackColor = false;
-            // 
-            // pictureBox8
-            // 
-            pictureBox8.Image = (Image)resources.GetObject("pictureBox8.Image");
-            pictureBox8.Location = new Point(48, 68);
-            pictureBox8.Name = "pictureBox8";
-            pictureBox8.Size = new Size(46, 50);
-            pictureBox8.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox8.TabIndex = 1;
-            pictureBox8.TabStop = false;
-            // 
-            // pictureBox4
-            // 
-            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
-            pictureBox4.Location = new Point(48, 152);
-            pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(46, 50);
-            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox4.TabIndex = 1;
-            pictureBox4.TabStop = false;
-            // 
-            // pictureBox7
-            // 
-            pictureBox7.Image = (Image)resources.GetObject("pictureBox7.Image");
-            pictureBox7.Location = new Point(48, 321);
-            pictureBox7.Name = "pictureBox7";
-            pictureBox7.Size = new Size(46, 50);
-            pictureBox7.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox7.TabIndex = 1;
-            pictureBox7.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(48, 235);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(46, 50);
-            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox2.TabIndex = 1;
-            pictureBox2.TabStop = false;
-            // 
-            // btnVehicles
-            // 
-            btnVehicles.BackColor = Color.Transparent;
-            btnVehicles.FlatAppearance.BorderSize = 0;
-            btnVehicles.FlatStyle = FlatStyle.Flat;
-            btnVehicles.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
-            btnVehicles.ForeColor = Color.Transparent;
-            btnVehicles.Location = new Point(100, 152);
-            btnVehicles.Name = "btnVehicles";
-            btnVehicles.Size = new Size(192, 50);
-            btnVehicles.TabIndex = 2;
-            btnVehicles.Text = "Vehicles";
-            btnVehicles.UseVisualStyleBackColor = false;
-            // 
-            // btnEmployees
-            // 
-            btnEmployees.BackColor = Color.Transparent;
-            btnEmployees.FlatAppearance.BorderSize = 0;
-            btnEmployees.FlatStyle = FlatStyle.Flat;
-            btnEmployees.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
-            btnEmployees.ForeColor = Color.Transparent;
-            btnEmployees.Location = new Point(100, 321);
-            btnEmployees.Name = "btnEmployees";
-            btnEmployees.Size = new Size(192, 50);
-            btnEmployees.TabIndex = 2;
-            btnEmployees.Text = "Employees";
-            btnEmployees.UseVisualStyleBackColor = false;
             // 
             // btnDashboard
             // 
@@ -151,12 +57,62 @@
             btnDashboard.FlatStyle = FlatStyle.Flat;
             btnDashboard.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
             btnDashboard.ForeColor = Color.Transparent;
-            btnDashboard.Location = new Point(100, 68);
+            btnDashboard.Image = (Image)resources.GetObject("btnDashboard.Image");
+            btnDashboard.Location = new Point(39, 53);
             btnDashboard.Name = "btnDashboard";
-            btnDashboard.Size = new Size(192, 50);
+            btnDashboard.Size = new Size(244, 50);
             btnDashboard.TabIndex = 2;
-            btnDashboard.Text = "Dashboard";
+            btnDashboard.Text = "        Dashboard";
+            btnDashboard.TextImageRelation = TextImageRelation.ImageBeforeText;
             btnDashboard.UseVisualStyleBackColor = false;
+            // 
+            // btnEmployees
+            // 
+            btnEmployees.BackColor = Color.Transparent;
+            btnEmployees.FlatAppearance.BorderSize = 0;
+            btnEmployees.FlatStyle = FlatStyle.Flat;
+            btnEmployees.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnEmployees.ForeColor = Color.Transparent;
+            btnEmployees.Image = (Image)resources.GetObject("btnEmployees.Image");
+            btnEmployees.Location = new Point(39, 321);
+            btnEmployees.Name = "btnEmployees";
+            btnEmployees.Size = new Size(253, 50);
+            btnEmployees.TabIndex = 2;
+            btnEmployees.Text = "        Employees";
+            btnEmployees.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnEmployees.UseVisualStyleBackColor = false;
+            // 
+            // btnVehicles
+            // 
+            btnVehicles.BackColor = Color.Transparent;
+            btnVehicles.FlatAppearance.BorderSize = 0;
+            btnVehicles.FlatStyle = FlatStyle.Flat;
+            btnVehicles.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnVehicles.ForeColor = Color.Transparent;
+            btnVehicles.Image = (Image)resources.GetObject("btnVehicles.Image");
+            btnVehicles.Location = new Point(30, 134);
+            btnVehicles.Name = "btnVehicles";
+            btnVehicles.Size = new Size(253, 50);
+            btnVehicles.TabIndex = 2;
+            btnVehicles.Text = "        Vehicles";
+            btnVehicles.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnVehicles.UseVisualStyleBackColor = false;
+            // 
+            // btnCustomers
+            // 
+            btnCustomers.BackColor = Color.Transparent;
+            btnCustomers.FlatAppearance.BorderSize = 0;
+            btnCustomers.FlatStyle = FlatStyle.Flat;
+            btnCustomers.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnCustomers.ForeColor = Color.Transparent;
+            btnCustomers.Image = (Image)resources.GetObject("btnCustomers.Image");
+            btnCustomers.Location = new Point(39, 232);
+            btnCustomers.Name = "btnCustomers";
+            btnCustomers.Size = new Size(253, 50);
+            btnCustomers.TabIndex = 2;
+            btnCustomers.Text = "        Customers";
+            btnCustomers.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnCustomers.UseVisualStyleBackColor = false;
             // 
             // MainMenu
             // 
@@ -171,20 +127,12 @@
             Name = "MainMenu";
             Text = "MainMenu";
             panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private Panel panel1;
-        private PictureBox pictureBox8;
-        private PictureBox pictureBox4;
-        private PictureBox pictureBox7;
-        private PictureBox pictureBox2;
         private Button btnCustomers;
         private Button btnDashboard;
         private Button btnEmployees;
