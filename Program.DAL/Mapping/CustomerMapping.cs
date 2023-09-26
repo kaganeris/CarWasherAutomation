6bb92cbe265c3196d7d95d62060e2259abfecae4
Kağan Eriş <kaan.ers@hotmail.com>
2023-09-26 11:03:39 +0300

    customer mapping update

---
+++ Program.DAL/Mapping/CustomerMapping.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Program.DATA.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program.DAL.Mapping
+{
+    public class CustomerMapping : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.HasKey(x => x.ID);
+
+            builder.HasMany(x => x.Vehicles).WithOne(x => x.Customer).HasForeignKey(x => x.CustomerID);
+        }
+    }
+}
