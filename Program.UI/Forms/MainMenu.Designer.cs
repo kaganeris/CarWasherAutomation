f99286fc55aa2b5c755252b16feff7a217ed8449
Mehmet Mücahid Bilge <mucahid.bilge@gmail.com>
2023-09-26 12:21:21 +0300

    FormUpdate

---
+++ Program.UI/Forms/MainMenu.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Program.UI
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenu));
+            panel1 = new Panel();
+            btnCustomers = new Button();
+            pictureBox8 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            btnVehicles = new Button();
+            btnEmployees = new Button();
+            btnDashboard = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.MidnightBlue;
+            panel1.Controls.Add(btnDashboard);
+            panel1.Controls.Add(btnEmployees);
+            panel1.Controls.Add(btnVehicles);
+            panel1.Controls.Add(btnCustomers);
+            panel1.Controls.Add(pictureBox8);
+            panel1.Controls.Add(pictureBox4);
+            panel1.Controls.Add(pictureBox7);
+            panel1.Controls.Add(pictureBox2);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(340, 846);
+            panel1.TabIndex = 0;
+            // 
+            // btnCustomers
+            // 
+            btnCustomers.BackColor = Color.Transparent;
+            btnCustomers.FlatAppearance.BorderSize = 0;
+            btnCustomers.FlatStyle = FlatStyle.Flat;
+            btnCustomers.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnCustomers.ForeColor = Color.Transparent;
+            btnCustomers.Location = new Point(100, 235);
+            btnCustomers.Name = "btnCustomers";
+            btnCustomers.Size = new Size(192, 50);
+            btnCustomers.TabIndex = 2;
+            btnCustomers.Text = "Customers";
+            btnCustomers.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.Image = (Image)resources.GetObject("pictureBox8.Image");
+            pictureBox8.Location = new Point(48, 68);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(46, 50);
+            pictureBox8.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox8.TabIndex = 1;
+            pictureBox8.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(48, 152);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(46, 50);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 1;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Image = (Image)resources.GetObject("pictureBox7.Image");
+            pictureBox7.Location = new Point(48, 321);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(46, 50);
+            pictureBox7.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox7.TabIndex = 1;
+            pictureBox7.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(48, 235);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(46, 50);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // btnVehicles
+            // 
+            btnVehicles.BackColor = Color.Transparent;
+            btnVehicles.FlatAppearance.BorderSize = 0;
+            btnVehicles.FlatStyle = FlatStyle.Flat;
+            btnVehicles.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnVehicles.ForeColor = Color.Transparent;
+            btnVehicles.Location = new Point(100, 152);
+            btnVehicles.Name = "btnVehicles";
+            btnVehicles.Size = new Size(192, 50);
+            btnVehicles.TabIndex = 2;
+            btnVehicles.Text = "Vehicles";
+            btnVehicles.UseVisualStyleBackColor = false;
+            // 
+            // btnEmployees
+            // 
+            btnEmployees.BackColor = Color.Transparent;
+            btnEmployees.FlatAppearance.BorderSize = 0;
+            btnEmployees.FlatStyle = FlatStyle.Flat;
+            btnEmployees.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnEmployees.ForeColor = Color.Transparent;
+            btnEmployees.Location = new Point(100, 321);
+            btnEmployees.Name = "btnEmployees";
+            btnEmployees.Size = new Size(192, 50);
+            btnEmployees.TabIndex = 2;
+            btnEmployees.Text = "Employees";
+            btnEmployees.UseVisualStyleBackColor = false;
+            // 
+            // btnDashboard
+            // 
+            btnDashboard.BackColor = Color.Transparent;
+            btnDashboard.FlatAppearance.BorderSize = 0;
+            btnDashboard.FlatStyle = FlatStyle.Flat;
+            btnDashboard.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnDashboard.ForeColor = Color.Transparent;
+            btnDashboard.Location = new Point(100, 68);
+            btnDashboard.Name = "btnDashboard";
+            btnDashboard.Size = new Size(192, 50);
+            btnDashboard.TabIndex = 2;
+            btnDashboard.Text = "Dashboard";
+            btnDashboard.UseVisualStyleBackColor = false;
+            // 
+            // MainMenu
+            // 
+            AutoScaleDimensions = new SizeF(10F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1279, 846);
+            Controls.Add(panel1);
+            Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            FormBorderStyle = FormBorderStyle.None;
+            IsMdiContainer = true;
+            Margin = new Padding(4);
+            Name = "MainMenu";
+            Text = "MainMenu";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private PictureBox pictureBox8;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox2;
+        private Button btnCustomers;
+        private Button btnDashboard;
+        private Button btnEmployees;
+        private Button btnVehicles;
+    }
+}
