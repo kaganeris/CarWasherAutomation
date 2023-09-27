b3fc9b3cdab8d77fbe4cc047d07d70e3bd724e99
Kağan Eriş <kaan.ers@hotmail.com>
2023-09-27 14:01:15 +0300

    seed data

---
+++ Program.DAL/SeedData/SeedEmployees.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Program.DATA.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program.DAL.NewFolder
+{
+    public class SeedEmployees : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.HasData
+                (
+                    new Employee { ID = 1, FirstName = "Kağan", LastName = "Eriş", Title = "Washer", Address = "Aydın/Türkiye" },
+                    new Employee { ID = 2, FirstName = "Mehmet Mücahid", LastName = "Bilge", Title = "Washer", Address = "Bursa/Türkiye" },
+                    new Employee { ID = 3, FirstName = "Ali Furkan", LastName = "Terzi", Title = "Washer", Address = "Ankara/Türkiye" },
+                    new Employee { ID = 4, FirstName = "İsmail", LastName = "Tosun", Title = "Washer", Address = "İzmir/Türkiye" },
+                    new Employee { ID = 5, FirstName = "Hakan", LastName = "Varyemez", Title = "Washer", Address = "İstanbul/Türkiye" },
+                    new Employee { ID = 6, FirstName = "Metin", LastName = "Tekin", Title = "Washer", Address = "Mersin/Türkiye" }
+                );
+        }
+    }
+}
---
+++ Program.DAL/SeedData/SeedSuppliers.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Program.DATA.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program.DAL.SeedData
+{
+    public class SeedSuppliers : IEntityTypeConfiguration<Supplier>
+    {
+        public void Configure(EntityTypeBuilder<Supplier> builder)
+        {
+            builder.HasData
+                (
+                    new Supplier { ID = 1, CompanyName = "" }
+                );
+        }
+    }
+}
